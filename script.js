21f79c5c518e0c1957d917d19cde170641f57283
Demandtech <demandwork247@gmail.com>
2022-06-22 09:48:44 +0100

    assignment done

---
+++ script.js
@@ -0,0 +1,7 @@
+let myName = document.querySelector(".name");
+let myHeight = document.querySelector(".height");
+let myCountry = document.querySelector(".country")
+let allElement = document.getElementsByTagName("*")
+myName.innerHTML="Name: Rasheed Adekunle";
+myHeight.innerHTML = "Height: 5'8";
+myCountry.innerHTML = "Country: Nigeria";
